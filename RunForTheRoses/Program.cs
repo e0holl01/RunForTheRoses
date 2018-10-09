58b68782c86e49da1813ec2cb22ea92bf176edfc
[e0holl01] <[erica.holland87@outlook.com]>
2018-10-09 14:28:25 -0400

    add system.IO namespace

--- RunForTheRoses/Program.cs
+++ RunForTheRoses/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace RunForTheRoses
 {
@@ -10,7 +11,7 @@ namespace RunForTheRoses
     {
       public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the 2016 Leading up to the Run for the Roses Repository.");
+            Console.WriteLine("Welcome to the 2016 Repository for Leading up to the Run for the Roses.");
   
         }
     }
