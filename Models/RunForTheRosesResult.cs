3c737fcc162649419a6725f7d22e3d44479cd1a1
Erica Holland <39104984+e0holl01@users.noreply.github.com>
2018-11-12 14:05:18 -0500

    Delete RunForTheRosesResult.cs

--- Models/RunForTheRosesResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RunForTheRoses.Models
-{
-    public class RunForTheRosesResult
-    {
-        public string Race { get; set; }
-        public string Place { get; set; }
-        public string Horse { get; set; }
-    }
-}
